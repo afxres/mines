1e5972be5ac0a9e5057c89c586e2f775177e1bfb
miko <stdarg@outlook.com>
2021-02-08 22:54:31 +0800

    Add basic definitions

---
+++ code/Mines.Abstractions/IMineGrid.cs
@@ -0,0 +1,43 @@
+﻿namespace Mikodev.Mines.Abstractions
+{
+    /// <summary>
+    /// 雷区
+    /// </summary>
+    public interface IMineGrid
+    {
+        /// <summary>
+        /// 游戏结束
+        /// </summary>
+        bool IsGameOver { get; }
+
+        /// <summary>
+        /// X 最大值
+        /// </summary>
+        int XMax { get; }
+
+        /// <summary>
+        /// Y 最大值
+        /// </summary>
+        int YMax { get; }
+
+        /// <summary>
+        /// 获取指定坐标的内容
+        /// </summary>
+        MineMark Get(int x, int y);
+
+        /// <summary>
+        /// 设置标记 (旗帜, 问号, 或清除标记)
+        /// </summary>
+        void Set(int x, int y);
+
+        /// <summary>
+        /// 翻开方块
+        /// </summary>
+        void Remove(int x, int y);
+
+        /// <summary>
+        /// 根据旗帜翻开周围方块
+        /// </summary>
+        void RemoveAll(int x, int y);
+    }
+}
---
+++ code/Mines.Abstractions/MineMark.cs
@@ -0,0 +1,38 @@
+﻿namespace Mikodev.Mines.Abstractions
+{
+    /// <summary>
+    /// UI 标记
+    /// </summary>
+    public enum MineMark
+    {
+        /// <summary>
+        /// 被翻开的方块 (数字 1 到 7 直接强制类型转换)
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// 未翻开的方块
+        /// </summary>
+        Tile = 0x80,
+
+        /// <summary>
+        /// 地雷
+        /// </summary>
+        Mine,
+
+        /// <summary>
+        /// 地雷 (错误点击)
+        /// </summary>
+        Trap,
+
+        /// <summary>
+        /// 旗帜
+        /// </summary>
+        Flag,
+
+        /// <summary>
+        /// 问号
+        /// </summary>
+        What,
+    }
+}
