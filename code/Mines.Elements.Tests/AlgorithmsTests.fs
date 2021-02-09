7d281592c73e4cc5e4b6c4ab1e6cdaca1de135b1
miko <stdarg@outlook.com>
2021-02-09 22:11:59 +0800

    Add index methods and unit tests

--- code/Mines.Elements.Tests/AlgorithmsTests.fs
+++ code/Mines.Elements.Tests/AlgorithmsTests.fs
@@ -16,4 +16,23 @@ type AlgorithmsTests() =
         Algorithms.shuffleInPlace target
         Assert.Equal<'T>(Array.sort source, Array.sort target)
         ()
-        
+
+    static member ``Index Data`` : obj array seq = seq {
+        let tie w h x y array = [| box w; box h; box x; box y; box array |]
+
+        yield (tie 1 1 0 0 Array.empty<(int * int)>)
+        yield (tie 9 1 1 0 [| 0, 0; 2, 0 |])
+        yield (tie 1 7 0 2 [| 0, 1; 0, 3 |])
+        yield (tie 9 9 0 0 [| 1, 0; 0, 1; 1, 1 |])
+        yield (tie 8 8 7 0 [| 6, 0; 6, 1; 7, 1 |])
+        yield (tie 6 6 0 5 [| 0, 4; 1, 4; 1, 5 |])
+        yield (tie 9 9 8 8 [| 7, 7; 8, 7; 7, 8 |])
+        yield (tie 7 7 3 3 [| 2, 2; 3, 2; 4, 2; 2, 3; 4, 3; 2, 4; 3, 4; 4, 4 |])
+    }
+
+    [<Theory>]
+    [<MemberData(nameof(AlgorithmsTests.``Index Data``))>]
+    member __.``MapAdjacentIndexes`` (w : int, h : int, x : int, y : int, expected : (int * int) array) =
+        let result = Algorithms.mapAdjacentIndexes w h x y (fun a b -> (a, b)) |> Seq.toArray
+        Assert.Equal<(int * int)>(expected, result)
+        ()
--- code/Mines.Elements/Algorithms.fs
+++ code/Mines.Elements/Algorithms.fs
@@ -3,7 +3,6 @@
 open System
 
 /// 洗牌算法 (直接操作数组)
-[<CompiledName("ShuffleInPlace")>]
 let shuffleInPlace (array : 'T array) =
     if array |> isNull then
         raise (ArgumentNullException(nameof(array)))
@@ -14,3 +13,39 @@ let shuffleInPlace (array : 'T array) =
         array.[i] <- array.[x]
         array.[x] <- a
     ()
+
+/// 展开二维数组索引到一维数组索引
+let flattenIndex (w : int) (h : int) (x : int) (y : int) =
+    if w < 0 || h < 0 || uint x > uint w || uint y > uint h then
+        raise (ArgumentOutOfRangeException())
+    y * w + x
+
+/// 映射指定位置周围的索引
+let mapAdjacentIndexes (w : int) (h : int) (x : int) (y : int) (f : int -> int -> 'T) =
+    if w < 0 || h < 0 || uint x > uint w || uint y > uint h then
+        raise (ArgumentOutOfRangeException())
+    // 判断上下左右是否存在
+    let l = x > 0
+    let r = x < w - 1
+    let t = y > 0
+    let b = y < h - 1
+
+    // 依次枚举左中右三个元素
+    let action line = seq {
+        if l then
+            yield f (x - 1) line
+        // 排除自身
+        if y <> line then
+            yield f x line
+        if r then
+            yield f (x + 1) line
+    }
+
+    // 依次枚举上中下三行
+    seq {
+        if t then
+            yield! action (y - 1)
+        yield! action y
+        if b then
+            yield! action (y + 1)
+    }
