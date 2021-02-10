34776e00534c50f3180ed6cf0ade227c59d99417
miko <stdarg@outlook.com>
2021-02-10 21:59:34 +0800

    Fix mine generate method

--- code/Mines.Elements/MineGrid.fs
+++ code/Mines.Elements/MineGrid.fs
@@ -2,7 +2,7 @@
 
 open System
 
-type MineGrid(w : int, h : int, x : int, y : int, count : int) =
+type MineGrid(w : int, h : int, count : int) =
     [<Literal>]
     let Mine : byte = 0xFFuy
 
@@ -16,11 +16,12 @@ type MineGrid(w : int, h : int, x : int, y : int, count : int) =
     do
         ensure 2 SizeMax w (nameof w)
         ensure 2 SizeMax h (nameof h)
-        ensure 0 (w - 1) x (nameof x)
-        ensure 0 (h - 1) y (nameof y)
         ensure 1 (w * h - 1) count (nameof count)
 
-    let items : byte array =
+    let generate x y =
+        ensure 0 (w - 1) x (nameof x)
+        ensure 0 (h - 1) y (nameof y)
+
         // 创建一个比总数小 1 的数组, 填充 N 个雷, 然后调用洗牌算法
         let origin : byte array = Array.zeroCreate (w * h - 1)
         Array.fill origin 0 count Mine
@@ -33,5 +34,17 @@ type MineGrid(w : int, h : int, x : int, y : int, count : int) =
             origin.[(i + 1)..]
         })
         assert(result.Length = w * h)
+
+        let flatten = Algorithms.flattenIndex w h
+
+        for m = 0 to w - 1 do
+            for n = 0 to h - 1 do
+                let i = flatten m n
+                if (result.[i] <> Mine) then
+                    let s = Algorithms.mapAdjacentIndexes w h m n flatten
+                    let n = s |> Seq.filter (fun x -> result.[x] = Mine) |> Seq.length
+                    result.[i] <- byte n
+                ()
+
         assert(result |> Array.filter ((=) Mine) |> Array.length = count)
         result
