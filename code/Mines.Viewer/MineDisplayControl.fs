00e6d9a721cc9d874a92bbd8178f6a0fa647ea74
miko <stdarg@outlook.com>
2021-02-11 18:02:51 +0800

    Add basic drawing logical

--- code/Mines.Viewer/MineDisplayControl.fs
+++ code/Mines.Viewer/MineDisplayControl.fs
@@ -3,6 +3,7 @@
 open Avalonia
 open Avalonia.Controls
 open Avalonia.Input
+open Avalonia.Interactivity
 open Avalonia.LogicalTree
 open Avalonia.Media
 open Mikodev.Mines.Abstractions
@@ -25,8 +26,30 @@ type MineDisplayControl() as me =
 
     let radius = 2.0
 
+    let handle (p : Point) (f : int -> int -> unit) =
+        if me.Bounds.Contains p then
+            let m = Math.Floor(p.X / (size + spacing)) |> int
+            let n = Math.Floor(p.Y / (size + spacing)) |> int
+            let rect = Rect(double m * (size + spacing), double n * (size + spacing), size, size)
+            if rect.Contains(p) then
+                f m n
+                top.Renderer.AddDirty me
+                ()
+            ()
+        ()
+
     let pointerPressedHandler = EventHandler<PointerPressedEventArgs>(fun _ e ->
-        top.Renderer.AddDirty me
+        let point = e.GetCurrentPoint me
+        let position = point.Position
+        handle position (fun x y ->
+            grid.Remove(x, y)
+            ())
+        ())
+
+    let tappedHandler = EventHandler<RoutedEventArgs>(fun _ e ->
+        ())
+
+    let doubleTappedHandler = EventHandler<RoutedEventArgs>(fun _ e ->
         ())
 
     let attached () =
@@ -37,20 +60,32 @@ type MineDisplayControl() as me =
         me.Height <- double h * size + double (h - 1) * spacing
 
         top <- me.FindLogicalAncestorOfType<TopLevel>()
+        top.Tapped.AddHandler tappedHandler
+        top.DoubleTapped.AddHandler doubleTappedHandler
         top.PointerPressed.AddHandler pointerPressedHandler
         ()
 
     let detached () =
+        top.Tapped.RemoveHandler tappedHandler
+        top.DoubleTapped.RemoveHandler doubleTappedHandler
         top.PointerPressed.RemoveHandler pointerPressedHandler
         top <- null
         grid <- null
         ()
 
     let render (d : DrawingContext) =
+        let number rect n =
+            d.DrawRectangle(Brushes.LightGray, null, rect, radius, radius)
+            let text = FormattedText(Text = string n, Typeface = Typeface.Default, FontSize = 14.0, TextAlignment = TextAlignment.Center, Constraint = Size(size, size))
+            d.DrawText(Brushes.DimGray, rect.TopLeft, text)
+            ()
+
         for m = 0 to w - 1 do
             for n = 0 to h - 1 do
                 let rect = Rect(double m * (size + spacing), double n * (size + spacing), size, size)
-                d.DrawRectangle(Brushes.Gray, null, rect, radius, radius)
+                match grid.Get(m, n) with
+                | MineMark.Tile -> d.DrawRectangle(Brushes.Gray, null, rect, radius, radius)
+                | n -> let x = int n in if (uint n) < 8u then number rect x else ()
                 ()
         ()
 
