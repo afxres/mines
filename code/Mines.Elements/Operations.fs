ef9a2c278f73a3daf6bef69a7b5fda4755b09520
miko <stdarg@outlook.com>
2021-02-24 20:27:46 +0800

    Add reduce function

--- code/Mines.Elements/Operations.fs
+++ code/Mines.Elements/Operations.fs
@@ -13,6 +13,11 @@ let set (grid : IMineGrid) x y mark =
         raise (ArgumentNullException (nameof grid))
     grid.Set(x, y, mark)
 
+let remove (grid : IMineGrid) x y =
+    if grid = null then
+        raise (ArgumentNullException (nameof grid))
+    grid.Remove(x, y)
+
 let toggle (grid : IMineGrid) x y =
     if grid = null then
         raise (ArgumentNullException (nameof grid))
@@ -23,3 +28,26 @@ let toggle (grid : IMineGrid) x y =
     | MineData.What -> set grid x y MineMark.None
     | _ -> ()
     ()
+
+let reduce (grid : IMineGrid) x y =
+    let f (g : IMineGrid) a b d =
+        if g.Status = MineGridStatus.Wait then
+            match d with
+            | MineData.Tile -> remove g a b
+            | MineData.What -> set g a b MineMark.None; remove g a b
+            | _ -> 0
+        else
+            0
+
+    if grid = null then
+        raise (ArgumentNullException (nameof grid))
+    let n = get grid x y |> int
+    if (n > 0 && n < 8) then
+        let l = Algorithms.adjacent grid.XMax grid.YMax x y |> Seq.map (fun (m, n) -> m, n, (get grid m n)) |> Seq.toList
+        let s = l |> List.sumBy (fun (_, _, d) -> if d = MineData.Flag then 1 else 0)
+        if (s >= n) then
+            l |> List.sumBy ((<|||) (f grid))
+        else
+            0
+    else
+        0
