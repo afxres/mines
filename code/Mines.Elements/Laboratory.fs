c403d4e1c2ccd45616f7cee51fdfe659ca44b043
miko <stdarg@outlook.com>
2021-02-26 23:26:22 +0800

    Add experimental function for laboratory module

--- code/Mines.Elements/Laboratory.fs
+++ code/Mines.Elements/Laboratory.fs
@@ -42,3 +42,86 @@ let remark (grid : IMineGrid) =
             y <- y + 1
         x <- x + 1
     ()
+
+let intersect (grid : IMineGrid) =
+    let w = grid.XMax
+    let h = grid.YMax
+
+    let flag = (=) MineData.Flag
+
+    let tile = int >> (<) 8
+
+    let free a = int a > 0 && int a < 8
+
+    let get = fun struct (x, y) -> Operations.get grid x y
+
+    let toggle = fun struct (x, y) -> Operations.set grid x y MineMark.Flag
+
+    let remove = fun struct (x, y) -> Operations.remove grid x y
+
+    let adjacent = fun struct (x, y) -> Algorithms.adjacent w h x y
+
+    let count f a =
+        let mutable n = 0
+        for i in adjacent a do
+            if get i |> f then
+                n <- n + 1
+        n
+
+    let choose f a = seq {
+        for i in adjacent a do
+            if get i |> f then
+                yield i
+    }
+
+    let reachable i = seq {
+        for a in adjacent i do
+            for b in choose free a do
+                if i <> b then
+                    yield b
+    }
+
+    let select a b =
+        let ta = choose tile a |> Set
+        let tb = choose tile b |> Set
+        let fa = choose flag a |> Set
+        let fb = choose flag b |> Set
+        let ua = Set.difference ta fa
+        let ub = Set.difference tb fb
+        let i = Set.intersect ua ub
+        if not (Set.isEmpty i) then
+            let ea = Set.difference ua i
+            let eb = Set.difference ub i
+            let va = get a
+            let vb = get b
+            let ra = int va - fa.Count
+            let rb = int vb - fb.Count
+            let ca = ra - ea.Count
+            let cb = rb - eb.Count
+            let c = max ca cb
+            if (c > 0) then
+                if not (Set.isEmpty ea) then
+                    if ra = ca then
+                        ()
+                    if c = ra then
+                        ea |> Seq.iter (remove >> ignore)
+                    if c = ca && Set.isEmpty eb then
+                        ea |> Seq.iter (toggle >> ignore)
+                if not (Set.isEmpty eb) then
+                    if rb = cb then
+                        ()
+                    if c = rb then
+                        eb |> Seq.iter (remove >> ignore)
+                    if c = cb && Set.isEmpty ea then
+                        eb |> Seq.iter (toggle >> ignore)
+        ()
+
+    for x = 0 to w - 1 do
+        for y = 0 to h - 1 do
+            let i = struct (x, y)
+            let n = get i
+            if (free n && count flag i < int n) then
+                let items = reachable i |> Set
+                for k in items do
+                    select i k
+    ()
