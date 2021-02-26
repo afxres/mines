b8b7dc35940a59a827e6094ce35f85363a3fa9e6
miko <stdarg@outlook.com>
2021-02-26 23:57:23 +0800

    Add button for laboratory function

--- code/Mines.Viewer/MainWindow.xaml.fs
+++ code/Mines.Viewer/MainWindow.xaml.fs
@@ -81,6 +81,7 @@ type MainWindow() as me =
         | "change" -> config g |> Async.StartImmediate
         | "remove" -> Laboratory.remove g
         | "remark" -> Laboratory.remark g
+        | "except" -> Laboratory.intersect g
         | _ -> ()
         ())
 
