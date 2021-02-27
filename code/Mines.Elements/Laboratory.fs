f88ea3f2c12aa99bad5fefa205d2ba7c1ce0a858
miko <stdarg@outlook.com>
2021-02-27 15:13:57 +0800

    Fix code style for laboratory module

--- code/Mines.Elements/Laboratory.fs
+++ code/Mines.Elements/Laboratory.fs
@@ -4,9 +4,10 @@ open Mikodev.Mines.Annotations
 open System.Collections.Generic
 
 let remove (grid : IMineGrid) =
+    let w = grid.XMax
+    let h = grid.YMax
+
     let invoke () =
-        let w = grid.XMax
-        let h = grid.YMax
         let mutable n = 0
         for x = 0 to w - 1 do
             for y = 0 to h - 1 do
@@ -21,22 +22,22 @@ let remove (grid : IMineGrid) =
     ()
 
 let remark (grid : IMineGrid) =
-    let f g =
-        fun struct (a, b) ->
-            if Operations.get g a b |> int > 8 then
-                Some struct (a, b)
-            else
-                None
+    let w = grid.XMax
+    let h = grid.YMax
+
+    let choose x y = seq {
+        for struct (a, b) in Algorithms.adjacent w h x y do
+            if Operations.get grid a b |> int > 8 then
+                yield struct (a, b)
+    }
 
     let invoke () =
-        let w = grid.XMax
-        let h = grid.YMax
         for x = 0 to w - 1 do
             for y = 0 to h - 1 do
-                let m = Operations.get grid x y
-                if int m >= 1 && int m <= 8 then
-                    let l = Algorithms.adjacent w h x y |> Seq.choose (f grid) |> Seq.toList
-                    if (l |> List.length = int m) then
+                let n = Operations.get grid x y
+                if int n >= 1 && int n <= 8 then
+                    let l = choose x y |> Seq.toList
+                    if (l |> List.length = int n) then
                         for struct (a, b) in l do Operations.set grid a b MineMark.Flag
 
     if grid.Status = MineGridStatus.Wait then
