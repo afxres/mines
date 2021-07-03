47e8430ae090ecd0e5ba35d4de412b021cacef73
miko <stdarg@outlook.com>
2021-07-03 22:50:56 +0800

    Fix range checking

--- code/Mines.Elements/Algorithms.fs
+++ code/Mines.Elements/Algorithms.fs
@@ -17,14 +17,14 @@ let flatten (w : int) (h : int) =
     if w < 0 || h < 0 then
         raise (ArgumentOutOfRangeException())
     let closure (x : int) (y : int) =
-        if uint x > uint w || uint y > uint h then
+        if uint x >= uint w || uint y >= uint h then
             raise (ArgumentOutOfRangeException())
         y * w + x
     closure
 
 /// 指定位置周围的索引
 let adjacent (w : int) (h : int) (x : int) (y : int) =
-    if w < 0 || h < 0 || uint x > uint w || uint y > uint h then
+    if w < 0 || h < 0 || uint x >= uint w || uint y >= uint h then
         raise (ArgumentOutOfRangeException())
 
     let l = if x > 0 then -1 else 0
